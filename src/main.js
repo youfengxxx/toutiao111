0e9261aa1ee604f1c2ffe0ef5d21a206f7b62a16
youfengxxx <1104499125@qq.com>
2022-09-13 22:39:34 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import './style/index.less'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
